7399b57606d6ee624b473f6ec6e9bdf7d1f62185
unknown <pc coordenao@pc-coordenacao.lab.unigran>
2024-04-26 15:19:27 -0400

    commit2

--- TrabalhoGit/src/main/java/br/unigran/Tela.java
+++ TrabalhoGit/src/main/java/br/unigran/Tela.java
@@ -3,10 +3,19 @@ package br.unigran;
 import javax.swing.*;
 
 public class Tela extends JFrame {
+    private JPanel tela = new JPanel();
+    private JButton buscar=new JButton("click");
     public Tela() {
         setTitle("Tela");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setSize(500,300);
         setLocationRelativeTo(null);
+        setVisible(true);
+        aloca();
+    }
+    public void aloca(){
+        add(tela);
+        tela.add(buscar);
     }
 }
+
