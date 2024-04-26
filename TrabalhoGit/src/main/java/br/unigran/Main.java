8ee87a5812d3a96c166d40f76f8f4ec0a779ca42
guilherme godoi <guilhermecostagodoi@gmail.com>
2024-04-26 14:36:33 -0300

    commit1

---
+++ TrabalhoGit/src/main/java/br/unigran/Main.java
@@ -0,0 +1,7 @@
+package br.unigran;
+
+public class Main {
+    public static void main(String[] args) {
+        Tela tela = new Tela();
+    }
+}
---
+++ TrabalhoGit/src/main/java/br/unigran/Tela.java
@@ -0,0 +1,12 @@
+package br.unigran;
+
+import javax.swing.*;
+
+public class Tela extends JFrame {
+    public Tela() {
+        setTitle("Tela");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(500,300);
+        setLocationRelativeTo(null);
+    }
+}
