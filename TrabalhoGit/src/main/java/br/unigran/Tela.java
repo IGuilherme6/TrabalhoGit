4040894bb4311a7661d4847a1c580c3e343288fd
guilherme godoi <guilhermecostagodoimail.com>
2024-04-26 15:25:17 -0300

    commit2

--- TrabalhoGit/src/main/java/br/unigran/Tela.java
+++ TrabalhoGit/src/main/java/br/unigran/Tela.java
@@ -1,12 +1,22 @@
 package br.unigran;
 
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionListener;
 
 public class Tela extends JFrame {
+
+    JLabel texto = new JLabel();
+    JTextArea painel = new JTextArea();
+
     public Tela() {
         setTitle("Tela");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setSize(500,300);
         setLocationRelativeTo(null);
+        setVisible(true);
+        add(painel,BorderLayout.PAGE_END);
+        painel.add(texto);
     }
+
 }
